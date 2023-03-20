504174f469e56ae82d4f36820361ab21694a5d58
Slumhunden <kroghbastin@gmail.com>
2023-03-20 13:20:47 +0100

    dom mani 1

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+//øvelser til intro af DOM//
+
+//Header//
+const header = document.createElement("h1");
+header.textContent = "develop. Preview. Ship.";
+document.querySelector("body").appendChild(header);
+//Paragraph//
+const ptag = document.createElement("p");
+ptag.textContent = "working";
+document.querySelector("body").appendChild(ptag);
